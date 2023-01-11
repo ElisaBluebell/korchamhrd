df5bf449b334e9c2c9cb4dc4f271b06b9c9df493
ElisaBluebell <selfishracer@hotmail.com>
2023-01-11 12:26:32 +0900

    230111 Elisa Bluebell

--- test.py
+++ test.py
@@ -153,43 +153,43 @@
 #     print("--- %s seconds ---" % {time.time() - start_time})
 #     print("shared_number=", end=""), print(shared_number)
 #     print("end of main")
-
-import threading
-import time
-
-shared_number = 0
-
-lock = threading.Lock()
-def thread_1(number):
-    global shared_number
-    print("number = ", end=""), print(number)
-    for i in range(number):
-        lock.acquire()  # Lock 획득
-        shared_number += 1
-        lock.release()  # Lock 해제
-
-def thread_2(number):
-    global shared_number
-    print("number = ", end=""), print(number)
-    for i in range(number):
-        lock.acquire()  # Lock 획득
-        shared_number += 1
-        lock.release()  # Lock 해제
-
-if __name__ == "__main__":
-
-    start_time = time.time()
-    t1 = threading.Thread(target=thread_1, args=(50000000, ))
-    t1.start()
-
-    t2 = threading.Thread(target=thread_2, args=(50000000, ))
-    t2.start()
-
-    threads = [t1, t2]
-
-    for t in threads:
-        t.join()
-
-    print("--- %s seconds ---" % {time.time() - start_time})
-    print("shared_number=", end=""), print(shared_number)
-    print("end of main")
+#
+# import threading
+# import time
+#
+# shared_number = 0
+#
+# lock = threading.Lock()
+# def thread_1(number):
+#     global shared_number
+#     print("number = ", end=""), print(number)
+#     for i in range(number):
+#         lock.acquire()  # Lock 획득
+#         shared_number += 1
+#         lock.release()  # Lock 해제
+#
+# def thread_2(number):
+#     global shared_number
+#     print("number = ", end=""), print(number)
+#     for i in range(number):
+#         lock.acquire()  # Lock 획득
+#         shared_number += 1
+#         lock.release()  # Lock 해제
+#
+# if __name__ == "__main__":
+#
+#     start_time = time.time()
+#     t1 = threading.Thread(target=thread_1, args=(50000000, ))
+#     t1.start()
+#
+#     t2 = threading.Thread(target=thread_2, args=(50000000, ))
+#     t2.start()
+#
+#     threads = [t1, t2]
+#
+#     for t in threads:
+#         t.join()
+#
+#     print("--- %s seconds ---" % {time.time() - start_time})
+#     print("shared_number=", end=""), print(shared_number)
+#     print("end of main")
