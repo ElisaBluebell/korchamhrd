ea81e9b01d817da8f4dbd440cc1faa772521718f
ElisaBluebell <selfishracer@hotmail.com>
2023-01-12 19:34:50 +0900

    230112 Elisa Bluebell

--- login_page.py
+++ login_page.py
@@ -1,12 +1,12 @@
-import sys
-import pymysql
-import webbrowser
 import datetime
+import pymysql
+import sys
+import webbrowser
 
-from PyQt5.QtCore import Qt, QSize
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QWidget, QLineEdit, QLabel, QPushButton, QMessageBox, QApplication
-from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QMessageBox, QPushButton, QWidget
 
 from main_page import MainPage
 
@@ -15,9 +15,12 @@ class LoginPage(QWidget):
 
     def __init__(self):
         super().__init__()
+        # 유저 DB를 담을 리스트
         self.user_info = []
+        # 첫 로그인 확인을 위한 변수
         self.log_in_count = 0
 
+        # ui 설정
         self.window_title = QLabel('광주인력개발원', self)
         self.user_id = QLabel('아이디', self)
         self.user_pw = QLabel('비밀번호', self)
@@ -31,31 +34,41 @@ class LoginPage(QWidget):
         self.link_btn = QPushButton(self)
 
         self.set_ui()
+        # 출석부 제작 함수
         self.set_attendance()
 
+    # 부르면 켜지등가
     @staticmethod
     def set_attendance():
+        # DB 임시저장용 변수
         temp = ''
+        # 커서 세팅
         conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
 
+        # 일단 읽어오고
         try:
             c.execute(f'SELECT * FROM korchamhrd.`{str(datetime.date.today())}`')
             temp = c.fetchall()
         except:
             pass
 
+        # 테이블이 만들어져있지 않을 경우 <오늘>을 이름으로 하는 출석부 테이블을 작성함
+        # 기존에 작성했던 user_account 테이블에서 로그인에만 필요한 id와 pw를 제외하고 만들어줌
         c.execute(f'''CREATE TABLE IF NOT EXISTS korchamhrd.`{str(datetime.date.today())}` (id INT NOT NULL, 
         user_name TEXT NOT NULL, message_reception INT NOT NULL, absence INT NOT NULL, tardy INT NOT NULL, 
         leave_early INT NOT NULL, period_cut INT NOT NULL, login_status INT NOT NULL, user_status INT NOT NULL, 
         curriculum_id INT NOT NULL, attend_time TEXT, cut_time TEXT, leave_time TEXT, return_time TEXT, 
         day_left INT NOT NULL, PRIMARY KEY (id))''')
 
+        # 오늘자 테이블이 이미 있을 경우 pass
         if temp:
             pass
         else:
+            # 오늘자 테이블이 없을 경우 이전 출석부에서 내용을 긁어와서
             c.execute(f'''SELECT * FROM korchamhrd.`{str(datetime.date.today() - datetime.timedelta(1))}`''')
-            temp = c.fetchall()
+            temp = list(c.fetchall())
+            # 변동사항을 적용하여 새로운 테이블에 삽입함
             for i in range(len(temp)):
                 c.execute(f'''INSERT INTO korchamhrd.`{str(datetime.date.today())}` VALUES ({temp[i][0]}, 
                 "{temp[i][1]}", {temp[i][2]}, {temp[i][3]}, {temp[i][4]}, {temp[i][5]}, {temp[i][6]}, 0, 
@@ -65,6 +78,7 @@ class LoginPage(QWidget):
         c.close()
         conn.close()
 
+    # 라벨 스타일 세팅
     def set_label(self):
         self.window_title.setFont(QtGui.QFont('D2Coding', 20))
         self.window_title.setAlignment(Qt.AlignCenter)
@@ -74,6 +88,7 @@ class LoginPage(QWidget):
 
         self.user_pw.setGeometry(20, 240, 65, 20)
 
+    # 라인 스타일 및 스위치 세팅
     def set_line(self):
         self.user_id_input.setGeometry(140, 180, 160, 20)
         self.user_id_input.returnPressed.connect(self.login_process_from_id_input)
@@ -82,6 +97,7 @@ class LoginPage(QWidget):
         self.user_pw_input.setGeometry(140, 240, 160, 20)
         self.user_pw_input.returnPressed.connect(self.login_process)
 
+    # 버튼 스타일 및 스위치 세팅
     def set_btn(self):
         self.login_btn.clicked.connect(self.login_process_with_btn)
         self.login_btn.setGeometry(100, 300, 60, 30)
@@ -94,6 +110,7 @@ class LoginPage(QWidget):
         self.link_btn.clicked.connect(self.open_web_browser)
         self.link_btn.setGeometry(20, 20, 192, 44)
 
+    # 창 기본 세팅
     def set_ui(self):
         self.setWindowIcon(QIcon('image/logo.png'))
 
@@ -102,24 +119,30 @@ class LoginPage(QWidget):
         self.set_btn()
         self.setFont(QtGui.QFont('D2Coding'))
 
-    # 아이디 입력 라인에딧을 통해 로그인할 경우 커서를 정상 위치에 옮겨두기 위해 비밀번호 입력 라인에딧으로 커서를 보냄
+    # 아이디 입력 라인에딧을 통해 로그인할 경우 커서를 기준점인 비밀번호 입력 라인에딧으로 커서를 보냄
     def login_process_from_id_input(self):
+        # 다음 항목(비밀번호 라인에딧)으로 커서를 보낸 후
         self.focusNextChild()
+        # 로그인 로직 실행
         self.login_process()
 
     # 로그인 버튼을 통해 로그인할 경우
     def login_process_with_btn(self):
+        # 이전 항목(비밀번호 라인에딧)으로 커서를 보냄
         self.focusPreviousChild()
         self.login_process()
 
     def login_process(self):
         # 비밀번호가 틀렸을 때 ID 확인과 메세지 중복출력 방지를 위해 비밀번호 일치 여부 변수 선언 및 초기화
         wrong_pw = 0
+
         conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
+
         # 로그인 상태 1 = 로그인, 0 = 로그아웃 상태, 상태 초기화
         c.execute(f'UPDATE korchamhrd.`{str(datetime.date.today())}` SET `login_status` = 0')
         conn.commit()
+
         # 0=유저 고유번호, 1=유저 계정 id, 2=유저 계정 비밀번호, 현재 반이 활성화 된 상태에 한해서 데이터를 불러옴
         c.execute('''SELECT a.id, a.user_id, a.user_pw 
         FROM korchamhrd.account_info AS a 
@@ -131,54 +154,77 @@ class LoginPage(QWidget):
         c.close()
         conn.close()
 
+        # id, pw 일치여부 판별을 위한 반복문
         for i in range(len(self.user_info)):
-            # user_id와 user_pw가 일치하는 값이 있을 경우 로그인 함수 실행
+            # user_id가 DB상의 id와 일치하는 경우
             if self.user_info[i][1] == self.user_id_input.text():
+                # 비밀번호 일치 여부 판별, 비밀번호도 일치할 경우
                 if self.user_info[i][2] == self.user_pw_input.text():
+                    # 유저 정보를 일치하는 정보로 치환한 후
                     self.user_info = list(self.user_info[i])
-                    # 로그인 이후 로그아웃 했을 때 커서를 아이디 창으로 보내기 위해 포커스를 앞으로 올림
+                    # 로그인 이후 로그아웃 했을 때 커서를 현 기준점인 pw에서 id로 보내기 위해 포커스를 앞으로 올림
                     self.focusPreviousChild()
                     self.log_in()
+                    # 작업 멈춤
                     break
 
+                # id만 일치하는 경우 비밀번호 틀림 판정
                 else:
                     QMessageBox.warning(self, '로그인 실패', '비밀번호를 확인하세요.')
                     self.user_pw_input.cursorPosition()
+                    # 비밀번호 틀림 변수를 참으로 바꿔주고
                     wrong_pw = 1
-                    # 밀번호 틀릴 시 자동으로 비밀번호가 전체선택되게
+                    # 비밀번호 틀릴 시 자동으로 비밀번호가 전체선택되게 한 번 흔들어줌
                     self.focusPreviousChild()
                     self.focusNextChild()
                     break
 
+        # 유저 정보 0번째 값이 튜플(기본형)이고(치환되지 않았고) 비밀번호 틀림 변수가 거짓일 경우 아이디 틀림 판정
         if type(self.user_info[0]) == tuple and wrong_pw == 0:
             QMessageBox.warning(self, '로그인 실패', '아이디를 확인하세요.')
-            # 아이디가 틀렸을 시 아이디 전체선택
+            # 아이디가 틀렸으니 커스를 기준점에서 한 칸 올려 아이디 칸 전체선택
             self.focusPreviousChild()
 
     def log_in(self):
         conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
+
+        # 로그인 한 아이디의 출석부 상 로그인 여부를 참으로 바꿔줌
         c.execute(f'UPDATE korchamhrd.`{str(datetime.date.today())}` SET `login_status` = 1 '
                   f'WHERE id = "{self.user_info[0]}"')
         conn.commit()
+
         c.close()
         conn.close()
+
         # 현재 불러온 db를 토대로 메인 페이지의 db 세팅
         main_page.set_db()
+
+        # 이전에 로그인을 실행한 적이 없을 경우
         if self.log_in_count == 0:
+            # 메인 페이지 ui를 깔아주고
             main_page.set_ui()
+            # 로그인 실행 여부를 참으로 바꿔줌
             self.log_in_count = 1
+
+        # 버튼의 텍스트 설정 및
         main_page.set_btn_text()
+        # 초기화 개념의 반응형 버튼 무력화
         main_page.set_btn_deactivate()
-        # 토요일, 일요일에는 로그인 버튼, 외출 버튼이 활성화되지 않음
+
+        # 토요일, 일요일에는 로그인 버튼, 외출 버튼이 활성화되지 않음, 그 외 요일에는 버튼 활성화
         if datetime.datetime.today().weekday() != 5 and datetime.datetime.today().weekday() != 6:
             main_page.set_btn_activate()
+
+        # 라벨 텍스트와 유저 상태를 현재 로그인 하는 유저에 맞게 설정
         main_page.set_label_text()
         main_page.set_user_status()
 
+        # 로그인 창의 id pw 입력 라인 초기화
         self.user_id_input.clear()
         self.user_pw_input.clear()
 
+        # 로그인 스택으로 이동
         widget.setCurrentIndex(1)
 
     @staticmethod
--- main_page.py
+++ main_page.py
@@ -1,11 +1,10 @@
 import datetime
-
 import pymysql
 
-from PyQt5.QtCore import Qt, QDate
-from PyQt5.QtGui import QTextCharFormat
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QCalendarWidget, QMessageBox
 from PyQt5 import QtGui
+from PyQt5.QtCore import QDate, Qt
+from PyQt5.QtGui import QTextCharFormat
+from PyQt5.QtWidgets import QCalendarWidget, QLabel, QMessageBox, QPushButton, QWidget
 from time import strftime
 
 from schedule_board import ScheduleBoard
@@ -16,7 +15,6 @@ class MainPage(QWidget):
     def __init__(self):
         super().__init__()
         self.user_info = []
-        self.user_curriculum = ''
         self.schedule_board = 0
 
         self.user_status = QLabel(self)
@@ -53,6 +51,7 @@ class MainPage(QWidget):
         c.close()
         conn.close()
 
+        # 유저의 현 상태와 커리큘럼 라벨 설정
         self.set_user_status()
         self.set_user_curriculum()
 
@@ -80,15 +79,19 @@ class MainPage(QWidget):
     def set_label_text(self):
         self.user_name.setText(f'{self.user_info[1]}님, 환영합니다.')
 
+        # 유저 번호 20만번 이하는 학생
         if self.user_info[0] < 200000:
+            # 학생의 경우 입실, 복귀, 외출, 퇴실 시간을 표시해주며
             self.attend_time.setText(f'입실 시간 | {self.user_info[10]}')
             self.comeback_time.setText(f'복귀 시간 | {self.user_info[13]}')
             self.cut_off_time.setText(f'외출 시간 | {self.user_info[11]}')
             self.leave_time.setText(f'퇴실 시간 | {self.user_info[12]}')
 
+            # 출결 여부도 함께 출력
             self.attend_status.setText(f'출석 {None} 결석 {self.user_info[3]}, 지각 {self.user_info[4]}, '
                                        f'조퇴 {self.user_info[5]}, 외출 {self.user_info[6]}')
 
+        # 교사의 경우 출퇴근 시간만 출력
         else:
             self.attend_time.setText(f'출근 시간 | {self.user_info[10]}')
             self.comeback_time.setText(f'퇴근 시간 | {self.user_info[12]}')
@@ -109,12 +112,15 @@ class MainPage(QWidget):
         self.attend_btn.clicked.connect(self.change_user_status)
         self.cut_off_btn.clicked.connect(self.cut_off)
 
+    # 반응형 버튼이 아닌 버튼들의 텍스트 세팅
     def set_btn_text(self):
         self.chat_btn.setText('상담')
         self.log_out_btn.setText('로그아웃')
         self.close_btn.setText('종료')
 
-    def set_btn_activate(self):        # 유저가 출근 or 입실 전일 경우
+    # 버튼 활성화 함수
+    def set_btn_activate(self):
+        # 유저가 출근 또는 입실 전인 경우의 버튼 세팅
         if self.user_info[8] == 0:
             self.attend_btn.setGeometry(60, 400, 80, 40)
 
@@ -128,9 +134,11 @@ class MainPage(QWidget):
         elif self.user_info[8] == 1:
             self.attend_btn.setGeometry(60, 400, 80, 40)
 
+            # 학생인 경우에 퇴실 텍스트 및 외출 버튼 출력함
             if self.user_info[0] < 200000:
                 self.attend_btn.setText('퇴실')
 
+                # 복귀 시간이 기본값인 경우 외출 버튼 활성화
                 if self.user_info[13] == 'NULL':
                     self.cut_off_btn.setGeometry(160, 400, 40, 40)
                     self.cut_off_btn.setText('외출')
@@ -138,10 +146,12 @@ class MainPage(QWidget):
             else:
                 self.attend_btn.setText('퇴근')
 
+        # 유저가 현재 외출 상태(교사는 외출 상태 진입 불가이기에 조건 x)인 경우 텍스트 복귀로 하여 버튼 활성화
         elif self.user_info[8] == 2:
             self.cut_off_btn.setGeometry(160, 400, 40, 40)
             self.cut_off_btn.setText('복귀')
 
+    # 버튼 비활성화, 입실(출근)과 외출 버튼을 사라지게 만들어 선택 자체가 불가능하게끔 함
     def set_btn_deactivate(self):
         self.attend_btn.setGeometry(0, 0, 0, 0)
         self.attend_btn.setText('')
@@ -155,13 +165,17 @@ class MainPage(QWidget):
         self.calendar.clicked.connect(self.schedule_management)
 
     def set_calendar_background(self):
+        # 텍스트 포멧 저장용 변수 설정
         fill_date_background = QTextCharFormat()
+        # 해당 변수 배경을 노란색으로
         fill_date_background.setBackground(Qt.yellow)
+        # 스케줄이 있는 날짜를 담아줄 리스트 선언
         scheduled_day = []
 
         conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
 
+        # 스케줄이 있는 날짜를 중복x로 받아옴
         c.execute('''SELECT DISTINCT DATE_FORMAT(the_day, "%Y-%m-%d") FROM korchamhrd.schedule_db 
         WHERE schedule_deleted=0 ORDER BY the_day''')
         temp = list(c.fetchall())
@@ -169,11 +183,15 @@ class MainPage(QWidget):
         c.close()
         conn.close()
 
+        # 스케줄 날짜 리스트에 값 삽입
         for i in range(len(temp)):
             scheduled_day.append(temp[i][0])
 
+        # 스케줄 데이 안에서
         for date in scheduled_day:
+            # 해당 날을 문자열 형태에서(문자열 형태로 저장한 DB에서 가져왔으므로) 받아온 뒤
             the_day = QDate.fromString(date, "yyyy-MM-dd")
+            # 받아온 날의 배경색을 채워줌
             self.calendar.setDateTextFormat(the_day, fill_date_background)
 
     def set_ui(self):
@@ -181,13 +199,17 @@ class MainPage(QWidget):
         self.set_label_text()
         self.set_btn()
         self.set_calendar()
+        # 기본 폰트 D2Coding
         self.setFont(QtGui.QFont('D2Coding'))
 
     def schedule_management(self):
+        # schedule_board.py 파일의 ScheduleBoard 클래스에 유저 정보와 달력에서 선택한 날을 상속받는 객체를 생성하고
         self.schedule_board = ScheduleBoard(self.user_info, self.calendar.selectedDate())
+        # 새 창 출력
         self.schedule_board.show()
 
     def set_user_status(self):
+        # 학생인 경우의 유저 상태
         if self.user_info[0] < 200000:
             if self.user_info[8] == 0:
                 self.user_status.setText('입실전')
@@ -210,30 +232,43 @@ class MainPage(QWidget):
                 
             else:
                 self.user_status.setText('퇴근')
-            
-    def set_user_curriculum(self):
-        self.user_curriculum = self.user_info[16]
-        if len(self.user_curriculum) > 14:
-            self.curriculum_title.setText(f'{self.user_curriculum[:12]}\n{self.user_curriculum[12:]}')
-        else:
-            self.curriculum_title.setText(self.user_curriculum)
 
+    def set_user_curriculum(self):
+        # 수업명을 받아오는 변수 선언
+        user_curriculum = self.user_info[16]
+        # 15글자 이상인 경우
+        if len(user_curriculum) > 14:
+            # 12글자에서 잘라줘서 맨 아랫쪽에 보기 흉하게 한두글자 내려가는 것을 방지함
+            self.curriculum_title.setText(f'{user_curriculum[:12]}\n{user_curriculum[12:]}')
+        else:
+            self.curriculum_title.setText(user_curriculum)
+
+    # 출결 상태 변경 함수
     def change_user_status(self):
         conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
 
+        # 입실 전 상태에서 해당 함수가 호출되었을 경우
         if self.user_info[8] == 0:
+            # 유저 상태를 출석으로 변경하고
             self.user_info[8] = 1
+
+            # 출석부 DB상에 시간을 적고 상태 변경
             c.execute(f'''UPDATE korchamhrd.`{str(datetime.date.today())}` SET user_status=1, 
             attend_time="{strftime('%I:%M')}" WHERE id={self.user_info[0]}''')
             conn.commit()
+
+            # 교수가 아닐 경우(교수의 과목 번호는 1) 입실, 교수의 경우 출근 메세지 출력
             if self.user_info[9] != 1:
                 QMessageBox.information(self, '입실', '입실하였습니다.')
             else:
                 QMessageBox.information(self, '출근', '출근하였습니다.')
 
+        # 입실 상태에서 해당 함수(입, 퇴실 버튼)가 호출된 경우
         elif self.user_info[8] == 1:
+            # 상태를 퇴실에 해당하는 3으로 변경 및 DB 적용
             self.user_info[8] = 3
+
             c.execute(f'''UPDATE korchamhrd.`{str(datetime.date.today())}` SET user_status=3, 
             leave_time="{strftime('%I:%M')}" WHERE id={self.user_info[0]}''')
             conn.commit()
@@ -246,6 +281,7 @@ class MainPage(QWidget):
         c.close()
         conn.close()
 
+        # 새로 바뀐 DB와 버튼, 라벨 등 새로고침
         self.set_db()
         self.set_btn_text()
         self.set_btn_deactivate()
@@ -253,20 +289,26 @@ class MainPage(QWidget):
         self.set_label_text()
         self.set_user_status()
 
+    # 외출 함수
     def cut_off(self):
         conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
 
+        # 유저가 입실 상태인 경우
         if self.user_info[8] == 1:
+            # 외출로 변경하고 외출 데이터를 DB에 작성
             self.user_info[8] = 2
+
             c.execute(f'''UPDATE korchamhrd.`{str(datetime.date.today())}` SET user_status=2, 
             cut_time="{strftime('%I:%M')}" WHERE id={self.user_info[0]}''')
             conn.commit()
 
             QMessageBox.information(self, '외출', '외출하였습니다.')
 
+        # 외출 상태에서 눌렀을 경우(복귀 버튼을 눌렀을 경우)
         elif self.user_info[8] == 2:
             self.user_info[8] = 1
+
             c.execute(f'''UPDATE korchamhrd.`{str(datetime.date.today())}` SET user_status=1, 
             return_time="{strftime('%I:%M')}", period_cut={self.user_info[6] + 1} WHERE id={self.user_info[0]}''')
             conn.commit()
@@ -284,9 +326,6 @@ class MainPage(QWidget):
         self.set_user_status()
 
     def chat(self):
-        pass
-
-    def do_nothing(self):
         pass
 
     def log_out(self):
