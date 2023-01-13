f7d098232d4359acbc6bbcc13d878eaa903b5736
ElisaBluebell <selfishracer@hotmail.com>
2023-01-13 21:08:02 +0900

    230112 Elisa Bluebell

--- chat_window.py
+++ chat_window.py
@@ -1,4 +1,8 @@
+import datetime
+import time
+
 import pymysql
+import threading
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QIcon
@@ -11,6 +15,7 @@ class ChatWindow(QWidget):
         super().__init__()
         self.user_info = user_info
         self.chat_db = []
+        self.chat_db_name = ''
 
         self.title = QLabel(self)
         self.opponent_class = QLabel(self)
@@ -30,6 +35,8 @@ class ChatWindow(QWidget):
         self.set_db()
 
         self.refresh_ui()
+        refresh_chat = threading.Thread(target=self.refresh_chat, daemon=True)
+        refresh_chat.start()
 
     def set_label(self):
         self.title.setText('일대일 상담')
@@ -42,6 +49,8 @@ class ChatWindow(QWidget):
 
     def set_line(self):
         self.chat_input.setGeometry(20, 460, 260, 20)
+        self.chat_input.returnPressed.connect(self.send_message)
+        self.chat_input.setMaxLength(15)
 
     def set_btn(self):
         self.send_chat_btn.clicked.connect(self.send_message)
@@ -71,16 +80,10 @@ class ChatWindow(QWidget):
 
             self.select_opponent_class.setGeometry(100, 60, 240, 20)
             for i in range(len(self.chat_db)):
-                print(1)
                 if self.chat_db[i][2] not in temp and self.chat_db[i][3] == 1:
-                    print(2)
                     temp.append(self.chat_db[i][2])
-                    print(3)
                     self.select_opponent_class.addItem(self.chat_db[i][2])
-                    print(4)
             self.select_opponent_class.currentTextChanged.connect(self.change_selected_name)
-
-            self.change_selected_name()
         self.select_opponent_name.currentTextChanged.connect(self.open_chat)
 
     def deactivate_ui(self):
@@ -103,11 +106,16 @@ class ChatWindow(QWidget):
         c = conn.cursor()
 
         c.execute('''SELECT a.id, a.user_name, b.class_name, b.class_status FROM korchamhrd.account_info AS a 
-        INNER JOIN korchamhrd.curriculum_db AS b ON a.curriculum_id=b.id ORDER BY b.id''')
+        INNER JOIN korchamhrd.curriculum_db AS b ON a.curriculum_id=b.id ORDER BY a.user_name''')
         self.chat_db = list(c.fetchall())
 
         c.close()
         conn.close()
+
+    def refresh_chat(self):
+        while True:
+            self.read_message()
+            time.sleep(0.3)
 
     def refresh_ui(self):
         self.deactivate_ui()
@@ -128,32 +136,50 @@ class ChatWindow(QWidget):
 
     def open_chat(self):
         self.create_chat()
-        # 학생
-        # if self.user_info < 200000:
+        self.read_message()
 
     def create_chat(self):
         conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
         c = conn.cursor()
-
         if self.user_info[0] < 200000:
-            c.execute(f'CREATE TABLE IF NOT EXISTS korchamhrd.`{self.select_opponent_name.currentText()}_'
-                      f'{self.user_info[1]}` (sender TEXT NOT NULL, content TEXT NOT NULL, time TEXT NOT NULL, '
-                      f'alarm INT NOT NULL)')
+            self.chat_db_name = self.select_opponent_name.currentText() + '_' + self.user_info[1]
+            c.execute(f'CREATE TABLE IF NOT EXISTS korchamhrd.`{self.chat_db_name}` (sender TEXT NOT NULL, '
+                      f'content TEXT NOT NULL, time TEXT NOT NULL, alarm INT NOT NULL)')
 
         else:
-            if self.select_opponent_name.currentText() and self.select_opponent_name.currentText() != self.user_info[1]:
-                c.execute(f'CREATE TABLE IF NOT EXISTS korchamhrd.`{self.user_info[1]}_'
-                          f'{self.select_opponent_name.currentText()}` (sender TEXT NOT NULL, content TEXT NOT NULL, '
-                          f'time TEXT NOT NULL, alarm INT NOT NULL)')
+            self.chat_db_name = self.user_info[1] + '_' + self.select_opponent_name.currentText()
+            c.execute(f'CREATE TABLE IF NOT EXISTS korchamhrd.`{self.chat_db_name}` (sender TEXT NOT NULL, '
+                      f'content TEXT NOT NULL, time TEXT NOT NULL, alarm INT NOT NULL)')
 
-            else:
-                pass
+        c.close()
+        conn.close()
 
     def send_message(self):
+        conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
+        c = conn.cursor()
+
+        c.execute(f'INSERT INTO korchamhrd.{self.chat_db_name} VALUES ("{self.user_info[1]}", '
+                  f'"{self.chat_input.text()}", "{datetime.datetime.now().time()}", 1)')
+        conn.commit()
+
+        c.close()
+        conn.commit()
+
+        self.chat_input.clear()
         self.read_message()
 
     def read_message(self):
-        self.chat_list.addItem(QListWidgetItem('a'))
+        self.chat_list.clear()
+
+        conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='korchamhrd')
+        c = conn.cursor()
+
+        c.execute(f'SELECT * FROM (SELECT * FROM korchamhrd.{self.chat_db_name} ORDER BY `time` DESC LIMIT 17) A ORDER BY `time` ASC')
+        chat_content = c.fetchall()
+        for i in range(len(chat_content)):
+            self.chat_list.addItem(QListWidgetItem(f'[{chat_content[i][2][:5]}] {chat_content[i][0]}: '
+                                                   f'{chat_content[i][1]}'))
 
     def close_window(self):
+        print(self.select_opponent_name.currentText())
         self.close()
